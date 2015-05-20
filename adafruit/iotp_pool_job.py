981c32eb95a043103b0333a1a422c05992456910
Francesco Zanitti <fzanitti@gmail.com>
2015-05-20 10:20:34 +0200

    added richtext job

--- adafruit/iotp_pool_job.py
+++ adafruit/iotp_pool_job.py
@@ -7,6 +7,19 @@ from StringIO import StringIO
 from PIL import Image
 
 
+def create_job(obj):
+    t = obj.get('type', None)
+    if t is None:
+        return None
+    if t == 'simple_text':
+        return SimpleTextJob(text=obj.get('text', ''), wrap=obj.get('wrap', False))
+    elif t == 'image':
+        return ImageJob(image=Image.open(StringIO(obj.get('image_file'))))
+    elif t == 'richtext':
+        return RichTextJob(commands=obj.get('commands', []))
+    return None
+
+
 class BaseJob(object):
     def __init__(self):
         self._id = uuid.uuid4()
@@ -35,7 +48,11 @@ class BaseJob(object):
     def run(self, printer):
         if self._state == 'queued':
             self._state = 'running'
-            self._with_printer(printer)
+            try:
+                self._with_printer(printer)
+            except Exception:
+                import logging
+                logging.exception("Error executing job {}", self.id_str)
             self._state = 'done'
             self._ts = int(time.time())
 
@@ -93,12 +110,131 @@ class ImageJob(BaseJob):
         return img
 
 
-def create_job(obj):
-    t = obj.get('type', None)
-    if t is None:
-        return None
-    if t == 'simple_text':
-        return SimpleTextJob(text=obj.get('text', ''), wrap=obj.get('wrap', False))
-    elif t == 'image':
-        return ImageJob(image=Image.open(StringIO(obj.get('image_file'))))
-    return None
+class RichTextJob(BaseJob):
+    def __init__(self, commands):
+        super(RichTextJob, self).__init__()
+        self._commands = commands
+
+    def _with_printer(self, printer):
+        for c in self._commands:
+            self._exec(c, printer)
+        self._commands = None
+
+    @staticmethod
+    def _exec(cmd, printer):
+        if isinstance(cmd, str):
+            code = cmd
+            args = None
+        else:
+            code = cmd['code']
+            args = cmd['args']
+        m = getattr(printer, code, None)
+        if m is None:
+            return
+        if args is None:
+            # only needs the self param
+            m()
+        else:
+            # call with cmd arg
+            m(*args)
+
+# all methods
+# ===========
+# timeoutSet(x)
+# timeoutWait
+# setTimes(p, f)
+# writeBytes(*args)
+# write(*data)
+# begin(heatTime=defaultHeatTime)
+# reset
+# setDefault
+# test
+# printBarcode(text, type)
+# setBarcodeHeight(val=50)
+# setPrintMode(mask)
+# unsetPrintMode(mask)
+# writePrintMode
+# normal
+# inverseOn
+# inverseOff
+# upsideDownOn
+# upsideDownOff
+# doubleHeightOn
+# doubleHeightOff
+# doubleWidthOn
+# doubleWidthOff
+# strikeOn
+# strikeOff
+# boldOn
+# boldOff
+# justify(value)
+# feed(x=1)
+# feedRows(rows)
+# flush
+# setSize(value)
+# underlineOn(weight=1)
+# underlineOff
+# printBitmap(w, h, bitmap, LaaT=False)
+# printImage(image, LaaT=False)
+# offline
+# online
+# sleep
+# sleepAfter(seconds)
+# wake
+# setLineHeight(val=32)
+# print(*args, **kwargs)
+# println(*args, **kwargs)
+
+# usable methods
+# ==============
+# reset
+# setDefault
+# printBarcode(text, type)
+# setBarcodeHeight(val=50)
+# normal
+# inverseOn
+# inverseOff
+# upsideDownOn
+# upsideDownOff
+# doubleHeightOn
+# doubleHeightOff
+# doubleWidthOn
+# doubleWidthOff
+# strikeOn
+# strikeOff
+# boldOn
+# boldOff
+# justify(value)
+# feed(x=1)
+# feedRows(rows)
+# setSize(value)
+# underlineOn(weight=1)
+# underlineOff
+# printBitmap(w, h, bitmap, LaaT=False)
+# printImage(image, LaaT=False)
+# setLineHeight(val=32)
+# print(*args, **kwargs)
+# println(*args, **kwargs)
+#
+# barcode types:
+# UPC_A   =  0
+# UPC_E   =  1
+# EAN13   =  2
+# EAN8    =  3
+# CODE39  =  4
+# I25     =  5
+# CODEBAR =  6
+# CODE93  =  7
+# CODE128 =  8
+# CODE11  =  9
+# MSI     = 10
+#
+# justify values:
+# c, r, l
+#
+# setSize values:
+# l, m, s
+
+# sample:
+# ['inverseOn', {'code':'println', 'args': ["helo!"]}, 'inverseOff',
+#  {'code':'print', 'args':["here are some", "lines of text."]}]
